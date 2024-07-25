fc94148340fd4999f16bf5239420c2b7aed4d7d8
Leonardo Troche <leo.m.troche@gmail.com>
2024-07-25 16:09:20 -0300

    chore: Update Tenor API key and refactor gifResponseMapper

--- client/src/services/getSearch.ts
+++ client/src/services/getSearch.ts
@@ -1,41 +1,70 @@
+// import type { MappedGifs } from "../types/types"
+
 import type { MappedGifs } from "../types/types"
 
-import { gifResponseMapper } from "../utils/gifResponseMapper"
+// import { gifResponseMapper } from "../utils/gifResponseMapper"
 
-interface GetSearchParams {
-  next: string
+// interface GetSearchParams {
+//   next: string
+//   query: string
+//   signal: AbortSignal
+// }
+
+// // const { VITE_TENOR_API_KEY: TENOR_API_KEY } = import.meta.env
+
+// const TENOR_API_KEY = getAPI_Key()
+
+// console.log(TENOR_API_KEY)
+
+// export async function getSearch({
+//   next,
+//   query,
+//   signal,
+// }: GetSearchParams): Promise<MappedGifs | undefined> {
+//   if (!query) {
+//     return
+//   }
+
+//   const URL = `https://tenor.googleapis.com/v2/search?q=${query}&key=${TENOR_API_KEY}&limit=${20}&pos=${next}`
+
+//   // const URL = `http://localhost:3000/query/q=${query}&pos=${next}`
+
+//   try {
+//     const resp = await fetch(URL, { signal })
+//     const data = await resp.json()
+
+//     if (resp.status !== 200) {
+//       console.log(resp.status)
+//       throw new Error("Error fetching gifs")
+//     }
+
+//     console.log(data)
+
+//     return gifResponseMapper(data)
+//   } catch (error) {
+//     if (signal.aborted) return
+
+//     console.log(error)
+//   }
+// }
+
+// const { data: API_KEY } = useFetch({ service: async () => fetch(URL) })
+
+// import { MappedGif } from "../types/types"
+
+interface GetGifs {
   query: string
+  next: string
   signal: AbortSignal
 }
 
-const { VITE_TENOR_API_KEY: TENOR_API_KEY } = import.meta.env
+export async function getSearch({ query, next, signal }: GetGifs): Promise<MappedGifs> {
+  const URL = `http://localhost:3000/search/${query}/${next}`
 
-export async function getSearch({
-  next,
-  query,
-  signal,
-}: GetSearchParams): Promise<MappedGifs | undefined> {
-  if (!query) {
-    return
-  }
+  console.log({ URL })
 
-  const URL = `https://tenor.googleapis.com/v2/search?q=${query}&key=${TENOR_API_KEY}&limit=${20}&pos=${next}`
+  const resp = await fetch(URL, { signal })
+  const data = await resp.json()
 
-  try {
-    const resp = await fetch(URL, { signal })
-    const data = await resp.json()
-
-    if (resp.status !== 200) {
-      console.log(resp.status)
-      throw new Error("Error fetching gifs")
-    }
-
-    console.log(data)
-
-    return gifResponseMapper(data)
-  } catch (error) {
-    if (signal.aborted) return
-
-    console.log(error)
-  }
+  return data
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-import express from "express"
 import cors from "cors"
-import { getUserInfoFromDatabase } from "./database/getUserInfoFromDatabase.js"
+import express from "express"
 import { giffyDb } from "./database/clientInstance.js"
 import { getGifComments } from "./database/getGifComments.js"
+import { getUserInfoFromDatabase } from "./database/getUserInfoFromDatabase.js"
+import { getSearchOnApi } from "./gifsAPI/getSearchOnApi.js"
 
 const app = express()
 const port = 3000
@@ -28,9 +29,32 @@ app.get(`/user/:userId`, async (req, res) => {
 
 app.get(`/comments/:gifId`, async (req, res) => {
   const { gifId } = req.params
-  const userInfo = await getGifComments(gifId)
-  res.json(userInfo)
+  const gifInfo = await getGifComments(gifId)
+  res.json(gifInfo)
 })
+
+app.get(`/search/:query/:next`, async (req, res) => {
+  const { query, next } = req.params
+  const queryData = await getSearchOnApi({ query, next })
+  res.json(queryData)
+})
+
+app.get(`/search/:query`, async (req, res) => {
+  const { query } = req.params
+  const queryData = await getSearchOnApi({ query, next: "" })
+  res.json(queryData)
+})
+
+// app.get('/fruit/:fruitName/:fruitColor', function(req, res) {
+//   var data = {
+//       "fruit": {
+//           "apple": req.params.fruitName,
+//           "color": req.params.fruitColor
+//       }
+//   };
+
+//   send.json(data);
+// });
 
 // Quiero cerrar la conexion de la base de datos al cerrar el servidor
 app.on("close", async () => {
