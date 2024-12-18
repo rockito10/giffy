d179cefea38fb3da32801e13821eecc2528e2ae1
Leonardo Troche <leo.m.troche@gmail.com>
2024-12-18 11:45:38 -0300

    Add tags

--- packages/client/src/pages/UploadPage.tsx
+++ packages/client/src/pages/UploadPage.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export default function UploadPage() {
 	const reader = new FileReader()
 	const [src, setSrc] = useState('')
+	const [tags, setTag] = useState<string[]>([])
+	const tagRef = useRef<HTMLInputElement | null>(null)
 
 	const handleOnDrop = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault()
@@ -20,21 +22,86 @@ export default function UploadPage() {
 		e.preventDefault()
 	}
 
+	const addTag = (tag: string) => {
+		if (tags.length > 10 && tag.trim().length <= 20) return // max amount of tags is 10 // max lenght is 20
+		setTag([...new Set([tag, ...tags])])
+	}
+
+	function handleAddTag(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+		e.preventDefault()
+
+		if (tagRef.current) addTag(tagRef.current.value.trim())
+	}
+
 	return (
 		<div>
 			<div className="flex gap-9">
-				<div className="relative size-1/2 md:size-1/3 border">
+				<div className="relative size-1/3 aspect-square">
+					{/* Drag & Drop */}
 					<div
-						className="size-1/2 md:size-1/3 aspect-square dashed-border"
+						className="aspect-square dashed-border min-w-[50%]"
 						onDrop={handleOnDrop}
 						onDragOver={handleOnDragOver}
 					/>
-					<img src={src} alt={'imagen'} className="absolute top-0 left-0 size-1/2 md:size-1/3" />
+
+					{/*  */}
+					{src && (
+						<img
+							src={src}
+							alt={'imagen'}
+							className="absolute top-1/2 left-1/2 w-3/4 -translate-x-1/2 -translate-y-1/2 rounded-md"
+						/>
+					)}
 				</div>
-				<textarea
-					className="h-16 w-full resize-none rounded-lg border border-white/70 bg-black p-2 text-white max-w-[50vw]"
-					placeholder="Add a description!"
-				/>
+
+				<form className="space-y-4">
+					<input
+						type="text"
+						className="h-16 w-full resize-none rounded-lg border border-white/70 bg-black p-2 text-white max-w-[50vw]"
+						placeholder="Add title!"
+					/>
+					<textarea
+						className="h-16 w-full resize-none rounded-lg border border-white/70 bg-black p-2 text-white max-w-[50vw]"
+						placeholder="Add a description!"
+					/>
+					<div className="flex gap-2">
+						<input
+							ref={tagRef}
+							type="text"
+							className="w-1/2 resize-none rounded-lg border border-white/70 bg-black p-2 text-white max-w-[50vw]"
+							placeholder="Add tag"
+						/>
+						<button
+							type="submit"
+							onClick={handleAddTag}
+							className="border border-white/70 bg-black p-2 hover:bg-white hover:text-black transition-colors rounded-lg"
+						>
+							+
+						</button>
+					</div>
+					{/* Contendor de Tags */}
+					<div className="flex gap-4 flex-wrap">
+						{tags.map((tag) => {
+							return (
+								<span
+									key={tag}
+									className="rounded-lg border px-2 py-1 transition-colors hover:bg-white/90 hover:text-black"
+								>
+									{tag}
+								</span>
+							)
+						})}
+					</div>
+
+					{src && (
+						<button
+							type="submit"
+							className="border border-purple-500/70 bg-purple-500 p-2 hover:bg-purple-600 transition-colors rounded-lg"
+						>
+							Upload
+						</button>
+					)}
+				</form>
 			</div>
 		</div>
 	)
