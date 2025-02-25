fdd3806f409005026227b34643fc393258ef3626
rockito10 <rockito10.mfd@gmail.com>
2025-02-25 20:19:34 -0300

    Update app-routes.ts

--- apps/api/src/routes/app-routes.ts
+++ apps/api/src/routes/app-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { authRoutes } from './auth-routes'
-import { commentsRoutes } from './comments-routes'
-import { likesRoutes } from './likes-routes'
-import { searchRoutes } from './search-routes'
-import { trendingRoutes } from './trending-routes'
+// import { authRoutes } from './auth-routes'
+// import { commentsRoutes } from './comments-routes'
+// import { likesRoutes } from './likes-routes'
+// import { searchRoutes } from './search-routes'
+// import { trendingRoutes } from './trending-routes'
 import { userRoutes } from './user-routes'
 
 export const giffyApiRouter = Router()
@@ -12,8 +12,8 @@ giffyApiRouter.get('/', (_req, res) => {
 	res.json({ message: 'Giffy API Online' })
 })
 giffyApiRouter.use('/user', userRoutes)
-giffyApiRouter.use('/search', searchRoutes)
-giffyApiRouter.use('/comments', commentsRoutes)
-giffyApiRouter.use('/likes', likesRoutes)
-giffyApiRouter.use('/auth/login', authRoutes)
-giffyApiRouter.use('/trending', trendingRoutes)
+// giffyApiRouter.use('/search', searchRoutes)
+// giffyApiRouter.use('/comments', commentsRoutes)
+// giffyApiRouter.use('/likes', likesRoutes)
+// giffyApiRouter.use('/auth/login', authRoutes)
+// giffyApiRouter.use('/trending', trendingRoutes)
