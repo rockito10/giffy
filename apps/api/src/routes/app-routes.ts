de64fdd16e8a125a065a4c429db0e264e1e845b9
rockito10 <rockito10.mfd@gmail.com>
2025-02-25 20:26:50 -0300

    Update app-routes.ts

--- apps/api/src/routes/app-routes.ts
+++ apps/api/src/routes/app-routes.ts
@@ -1,5 +1,9 @@
 import express from 'express'
-
+// import { authRoutes } from './auth-routes'
+// import { commentsRoutes } from './comments-routes'
+// import { likesRoutes } from './likes-routes'
+import { searchRoutes } from './search-routes'
+// 
 export const giffyApiRouter = express.Router()
 
 giffyApiRouter.get('/', (_req, res) => {
