83f5e3bfcc98847270ef0ef91df70a8c4dddd7b3
rockito10 <rockito10.mfd@gmail.com>
2025-02-26 20:45:46 -0300

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:3000',
+				target: 'https://giffy-server.vercel.app',
 				changeOrigin: true,
 			},
 		},
