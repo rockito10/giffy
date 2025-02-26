9ce8ac26901fac401c6a8ff6ccb195b5c165494b
rockito10 <rockito10.mfd@gmail.com>
2025-02-26 09:14:48 -0300

    vercel json

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -5,3 +5,9 @@ const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`)
 })
+
+// CREA UNA SERVERLESS FUNCTION QUE EJECUTE MI SERVER PARA SUBURKI A VER
+
+export const handler = async (event: any, context: any) => {
+	return app.handler(event, context)
+}
