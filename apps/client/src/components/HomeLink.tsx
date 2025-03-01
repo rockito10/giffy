3cc182f30078e011bc2838937fd9fc7b0783f8a7
rockito10 <rockito10.mfd@gmail.com>
2025-03-01 12:25:19 -0300

    package json

--- apps/client/src/components/HomeLink.tsx
+++ apps/client/src/components/HomeLink.tsx
@@ -1,15 +1,14 @@
-import type { HTMLAttributes } from "react";
-import { Link } from "wouter";
+import type { HTMLAttributes } from 'react'
+import { Link } from 'wouter'
 
 interface Props extends HTMLAttributes<HTMLAnchorElement> {}
 
 export function HomeLink(props: Props) {
-  return (
-    <Link to="/">
-      <span className="font-bold text-[1.35rem] text-gradient flex items-center gap-1 rounded p-2 transition-transform hover:scale-125">
-        GIFFY
-      </span>
-    </Link>
-    {""}
-  );
+	return (
+		<Link to="/">
+			<span className='flex items-center gap-1 rounded p-2 font-bold text-[1.35rem] text-gradient transition-transform hover:scale-125'>
+				GIFFY
+			</span>
+		</Link>
+	)
 }
