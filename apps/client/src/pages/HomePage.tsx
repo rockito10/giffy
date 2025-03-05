26fa24890ff69f170b85028d65e8f992fcb14d3d
rockito10 <rockito10.mfd@gmail.com>
2025-03-05 11:28:50 -0300

    Update HomePage.tsx

--- apps/client/src/pages/HomePage.tsx
+++ apps/client/src/pages/HomePage.tsx
@@ -7,37 +7,26 @@ export default function HomePage() {
 
   const userID = getSavedUserId();
 
-  return (
-    <div className="flex flex-col items-start gap-8">
-      <h2 className="font-medium text-5xl text-gradient">
-        {userID ? "FAVORITE GIFS" : "TRENDING GIFS"}
-      </h2>
+	return (
+		<div className="flex flex-col items-start gap-4">
+			<h2 className="font-medium text-5xl text-gradient">
+				{userID ? 'FAVORITE GIFS' : 'TRENDING GIFS'}
+			</h2>
 
-      <div className="home-container w-full">
-        {
-          <div className="home-box">
-            {userID ? <FavoriteGifs /> : <TrendingGifs />}
-          </div>
-        }
-      </div>
+			<div className="home-container w-full">
+				{<div className="home-box">{userID ? <FavoriteGifs /> : <TrendingGifs />}</div>}
 
-      {userID ? (
-        <div className="flex w-full flex-col gap-6">
-          <h2 className="font-medium text-5xl text-gradient">
-            {"TRENDING GIFS"}
-          </h2>
-          <div className="home-container w-full">
-            {
-              <div className="home-box">
-                {" "}
-                {userID ? <TrendingGifs /> : <div />}{" "}
-              </div>
-            }
-          </div>
-        </div>
-      ) : (
-        <div />
-      )}
-    </div>
-  );
+				
+			</div>
+
+			<h2 className="font-medium text-5xl text-gradient">
+				{userID ? 'TRENDING GIFS' : ''}
+			</h2>
+			<div className="home-container w-full">
+			
+			
+				{<div className="home-box"> {userID ? <TrendingGifs /> : <div />} </div>}
+				</div>
+		</div>
+	)
 }
