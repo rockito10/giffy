ea4542ef373b40208a0cb9ddf38c0c883af172e4
rockito10 <rockito10.mfd@gmail.com>
2025-03-26 20:42:20 -0300

    Update cloudinary.ts

--- apps/api/src/config/cloudinary.ts
+++ apps/api/src/config/cloudinary.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary'
 	// Configuration
 	cloudinary.config({
 		cloud_name: 'dm5rzwoa3',
-		api_key: '844888962214969',
-		api_secret: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API secret
+		api_key: process.env.CLOUDINARY_API_KEY,
+		api_secret: process.env.CLODUINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 	})
 })()
